e3b81e533b834902724314324c679b7d71df1d6a
chuchko.al <chuchko.al@gmail.com>
2021-03-21 13:49:26 +0200

    Add project files.

---
+++ LuckyTicketProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LuckyTicketProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
